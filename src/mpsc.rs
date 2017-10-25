692ef6886ac40b54779f64117b30b8ba89b4b571
Andrew Cann <shum@canndrew.org>
2017-10-25 17:43:28 +0800

    Fix error type of mpsc::UnboundedSender

--- src/mpsc.rs
+++ src/mpsc.rs
@@ -1,33 +1,19 @@
 //! channels in the futures-rs crate cannot error, yet they return () for their error type for some
 //! stupid reason. This is a wrapper around futures-rs unbounded channels which removes the error.
 
-use futures::{self, Stream, Sink, Async, AsyncSink};
+use futures::{self, Stream, Async};
 use void::Void;
 
-pub struct UnboundedSender<T> {
-    inner: futures::sync::mpsc::UnboundedSender<T>,
-}
+pub use futures::sync::mpsc::{UnboundedSender, SendError};
 
+#[derive(Debug)]
 pub struct UnboundedReceiver<T> {
     inner: futures::sync::mpsc::UnboundedReceiver<T>,
 }
 
 pub fn unbounded<T>() -> (UnboundedSender<T>, UnboundedReceiver<T>) {
     let (tx, rx) = futures::sync::mpsc::unbounded();
-    (UnboundedSender { inner: tx }, UnboundedReceiver { inner: rx })
-}
-
-impl<T> Sink for UnboundedSender<T> {
-    type SinkItem = T;
-    type SinkError = Void;
-
-    fn start_send(&mut self, item: T) -> Result<AsyncSink<T>, Void> {
-        Ok(unwrap!(self.inner.start_send(item)))
-    }
-
-    fn poll_complete(&mut self) -> Result<Async<()>, Void> {
-        Ok(unwrap!(self.inner.poll_complete()))
-    }
+    (tx, UnboundedReceiver { inner: rx })
 }
 
 impl<T> Stream for UnboundedReceiver<T> {
